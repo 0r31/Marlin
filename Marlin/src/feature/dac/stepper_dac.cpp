34bbd839b0b9f0b4ba8e1db0458296e735683947
orel <orel@dagoma.fr>
2020-11-10 17:43:11 +0100

    Reset dac stepper values if config values differ from the current ones

--- Marlin/src/feature/dac/stepper_dac.cpp
+++ Marlin/src/feature/dac/stepper_dac.cpp
@@ -51,7 +51,10 @@ int StepperDAC::init() {
   mcp4728.setVref_all(DAC_STEPPER_VREF);
   mcp4728.setGain_all(DAC_STEPPER_GAIN);
 
-  if (mcp4728.getDrvPct(0) < 1 || mcp4728.getDrvPct(1) < 1 || mcp4728.getDrvPct(2) < 1 || mcp4728.getDrvPct(3) < 1 ) {
+  if (ABS(dac_channel_pct[0] - mcp4728.getDrvPct(0)) > 1 \
+   || ABS(dac_channel_pct[1] - mcp4728.getDrvPct(1)) > 1 \
+   || ABS(dac_channel_pct[2] - mcp4728.getDrvPct(2)) > 1 \
+   || ABS(dac_channel_pct[3] - mcp4728.getDrvPct(3)) > 1 ) {
     mcp4728.setDrvPct(dac_channel_pct);
     mcp4728.eepromWrite();
   }
@@ -72,8 +75,8 @@ void StepperDAC::set_current_percent(const uint8_t channel, float val) {
   set_current_value(channel, _MIN(val, 100.0f) * (DAC_STEPPER_MAX) / 100.0f);
 }
 
-static float dac_perc(int8_t n) { return 100.0 * mcp4728.getValue(dac_order[n]) * RECIPROCAL(DAC_STEPPER_MAX); }
-static float dac_amps(int8_t n) { return mcp4728.getDrvPct(dac_order[n]) * (DAC_STEPPER_MAX) * 0.125 * RECIPROCAL(DAC_STEPPER_SENSE); }
+static float dac_perc(int8_t n) { return mcp4728.getDrvPct(dac_order[n]); }
+static float dac_amps(int8_t n) { return mcp4728.getValue(dac_order[n]) / 1000.0; }
 
 uint8_t StepperDAC::get_current_percent(const AxisEnum axis) { return mcp4728.getDrvPct(dac_order[axis]); }
 void StepperDAC::set_current_percents(xyze_uint8_t &pct) {
